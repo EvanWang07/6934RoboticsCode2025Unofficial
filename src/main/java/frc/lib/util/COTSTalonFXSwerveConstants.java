ba89c20db0a7037a33b3977bc8d53d6ba52b9c23
Evan Wang <evwang07@gmail.com>
2025-02-27 17:46:29 -0800

    Correct swerve wheel diameter once again

--- src/main/java/frc/lib/util/COTSTalonFXSwerveConstants.java
+++ src/main/java/frc/lib/util/COTSTalonFXSwerveConstants.java
@@ -264,7 +264,7 @@ public class COTSTalonFXSwerveConstants {
         public static final class MK4i{
             /** Swerve Drive Specialties - MK4i Module (Falcon 500)*/
             public static final COTSTalonFXSwerveConstants Falcon500(double driveGearRatio){
-                double wheelDiameter = Units.inchesToMeters(3.85);
+                double wheelDiameter = Units.inchesToMeters(3.8);
         
                 /** (150 / 7) : 1 */
                 double angleGearRatio = ((150.0 / 7.0) / 1.0);
@@ -281,7 +281,7 @@ public class COTSTalonFXSwerveConstants {
 
             /** Swerve Drive Specialties - MK4i Module (Kraken X60)*/
             public static final COTSTalonFXSwerveConstants KrakenX60(double driveGearRatio){
-                double wheelDiameter = Units.inchesToMeters(3.85);
+                double wheelDiameter = Units.inchesToMeters(3.8);
         
                 /** (150 / 7) : 1 */
                 double angleGearRatio = ((150.0 / 7.0) / 1.0);
